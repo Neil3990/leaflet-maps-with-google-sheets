bd7806ffc43dad305f5cf2657c392de4555cc9a4
Neil3990 <42565902+Neil3990@users.noreply.github.com>
2018-10-08 15:02:33 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1IrYFhFvG1jf6kZ6G4_bqIJz-BnpS6eVVlfJ8zp62ghk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Db9kwZKNPdQajWDmwru5hvrLayJXIcGY3u3CNLPOQeg/edit#gid=1473536287';
